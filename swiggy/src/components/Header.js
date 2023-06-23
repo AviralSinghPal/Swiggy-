c7027d0130533efbe78891da184e70835d55d5ec
Your Name <you@example.com>
2023-06-23 22:24:32 +0530

    initailizing componenets of our app

---
+++ swiggy/src/components/Header.js
@@ -0,0 +1,21 @@
+
+var Header = () =>
+{
+    return(
+        <>
+        <div className="header">
+        <img className="logo"alt="logo" src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"/>
+        <div className= "nav-item">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+                
+            </ul>
+        </div>
+        </div>
+        </>);
+}
+
+export default Header;
